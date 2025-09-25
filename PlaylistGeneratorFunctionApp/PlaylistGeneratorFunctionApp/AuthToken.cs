9d0828a35678f6f60ea96c5814b01ee65ac327de
haggau15 <haggau94@gmail.com>
2025-09-25 10:47:13 +0200

    Song info and a playlist can be retrieved from the spotify API

---
+++ PlaylistGeneratorFunctionApp/PlaylistGeneratorFunctionApp/AuthToken.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+public class SpotifyAuthHelper
+{
+    private readonly string _clientId;
+    private readonly string _clientSecret;
+    private readonly string _refreshToken;
+
+    public SpotifyAuthHelper(string clientId, string clientSecret, string refreshToken)
+    {
+        _clientId = clientId;
+        _clientSecret = clientSecret;
+        _refreshToken = refreshToken;
+    }
+
+    public async Task<string> GetAccessTokenAsync()
+    {
+        using var client = new HttpClient();
+
+        // Create Basic Auth header
+        var authHeader = Convert.ToBase64String(Encoding.UTF8.GetBytes($"{_clientId}:{_clientSecret}"));
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", authHeader);
+
+        var body = new StringContent(
+            $"grant_type=refresh_token&refresh_token={_refreshToken}",
+            Encoding.UTF8,
+            "application/x-www-form-urlencoded"
+        );
+
+        var response = await client.PostAsync("https://accounts.spotify.com/api/token", body);
+        var json = await response.Content.ReadAsStringAsync();
+
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception($"Failed to refresh token: {response.StatusCode} - {json}");
+        }
+
+        var doc = JsonDocument.Parse(json);
+        return doc.RootElement.GetProperty("access_token").GetString();
+    }
+}
--- PlaylistGeneratorFunctionApp/PlaylistGeneratorFunctionApp/Function1.cs
+++ PlaylistGeneratorFunctionApp/PlaylistGeneratorFunctionApp/Function1.cs
@@ -2,12 +2,19 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using System.Net.Http.Headers;
 
 namespace PlaylistGeneratorFunctionApp;
 
 public class Function1
 {
     private readonly ILogger<Function1> _logger;
+    static readonly SpotifyAuthHelper authHelper = new(
+        "dadcc3e1920f4fb78f62e6704e233a0f",
+        "8d2958f6009b44a2ba45646d55c0c023",
+        "AQAfWV4EtagD6U7kVzVPtlRn1mGFMvSRmEDdRxmrFgVmiEXI3eG-oC-KUQUJB1Vzphvsslp78v-KCQ8skdjO2PigmtQjQ5m9PsXISdmgB7wr0O9HipCySPnoiUDZdL1JMXA" // obtained during initial OAuth login
+    );
+
 
     public Function1(ILogger<Function1> logger)
     {
@@ -15,9 +22,35 @@ public class Function1
     }
 
     [Function("Function1")]
-    public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+    public async Task<IActionResult> GetTrackInfo([HttpTrigger(AuthorizationLevel.Function, "get", Route = "Obama")] HttpRequest req)
     {
+        string accessToken = await authHelper.GetAccessTokenAsync();
+
+        using var client = new HttpClient();
+        client.DefaultRequestHeaders.Authorization =
+            new AuthenticationHeaderValue("Bearer", accessToken);
+
+        var response = await client.GetAsync($"https://api.spotify.com/v1/tracks/4uLU6hMCjMI75M1A2tKUQC");
+        string json = await response.Content.ReadAsStringAsync();
+
+        Console.WriteLine(json);
         _logger.LogInformation("C# HTTP trigger function processed a request.");
-        return new OkObjectResult("Welcome to Azure Functions!");
+        return new OkObjectResult(json);
+    }
+
+    [Function("Function2")]
+    public static async Task<IActionResult> GetPlaylist(string id,[HttpTrigger(AuthorizationLevel.Function, "get", Route = "Obamna")] HttpRequest req)
+    {
+        string accessToken = await authHelper.GetAccessTokenAsync();
+
+        using var client = new HttpClient();
+        client.DefaultRequestHeaders.Authorization =
+            new AuthenticationHeaderValue("Bearer", accessToken);
+
+        var response = await client.GetAsync($"https://api.spotify.com/v1/playlists/"+id);
+        string json = await response.Content.ReadAsStringAsync();
+
+        Console.WriteLine(json);
+        return new OkObjectResult(json);
     }
 }
